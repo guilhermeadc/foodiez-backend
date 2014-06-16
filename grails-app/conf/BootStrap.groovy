4be19e45a9524659ad16bd4720de58d04676e98b
Guilherme Del Cantoni <guilhermeadc@gmail.com>
2014-06-15 23:50:54 -0300

    Adicionado carga de dados para testes

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,38 +1,51 @@
-import br.com.beezu.foodiez.*
-import br.com.beezu.foodiez.domain.Restaurant
+import br.com.beezu.foodiez.domain.*
 
 class BootStrap {
 
     def init = { servletContext ->
-        //Restaurant: 
-        def restaurant = new Restaurant(venueId: "4caca9ce14c337041d47f23b")
-        restaurant.menu.name = "Menu Principal"
-        restaurant.menu.description = "O Outback Steakhouse é um restaurante informal, construído e decorado em estilo que remete ao interior da Austrália."
-        insertMenuSection(restaurant.menu, ["APERITIVOS", "SALADAS ESPECIAIS", "FAVORITOS OUTBACK", "STEAKS OUTBACK", "NOSSA PESCARIA", "MASSAS", "PREMIUM BURGERS AND SANDWICHES", "SOBREMESAS IRRESISTÍVEIS", "LUNCH MENU", "KID'S MENU", "BEBIDAS", "VINHOS"])
-        restaurant.save()        
 
-
-        new Restaurant(venueId: "4f885571e4b0e81f666d9662").save()
-        new Restaurant(venueId: "4d19f3da1356a093c7b6ed82").save()
-        new Restaurant(venueId: "50d37b3ae4b04d6aedfd5bba").save()
-        new Restaurant(venueId: "50aff159e4b07ba2ce458242").save()
-        new Restaurant(venueId: "4bad2d03f964a520df353be3").save()
-        new Restaurant(venueId: "51b66034498e86980c7c3cac").save()
-        new Restaurant(venueId: "4b9245a8f964a52057ef33e3").save()
-        new Restaurant(venueId: "4bd394efa8b3a593aa666a5f").save()
-        new Restaurant(venueId: "4bd394efa8b3a593aa666a5f").save()
+        createRestaurant("4caca9ce14c337041d47f23b").save()
+        createRestaurant("4f885571e4b0e81f666d9662").save()            
+        createRestaurant("4d19f3da1356a093c7b6ed82").save()
+        createRestaurant("50d37b3ae4b04d6aedfd5bba").save()
+        createRestaurant("50aff159e4b07ba2ce458242").save()
+        createRestaurant("4bad2d03f964a520df353be3").save()
+        createRestaurant("51b66034498e86980c7c3cac").save()
+        createRestaurant("4b9245a8f964a52057ef33e3").save()
+        createRestaurant("4bd394efa8b3a593aa666a5f").save()
+        createRestaurant("4bd394efa8b3a593aa666a5f").save()
     }
 
     def destroy = {
     }
 
-    def insertMenuSection(menu, sectionNames) {
-        assert menu != null, "Parâmetro [menu] não pode ser nulo"
-        assert sectionNames instanceof List , "Parâmetro [sectionNames] inválido"
-        
+    def createRestaurant(venueId) {
+        def restaurant = new Restaurant(venueId: venueId)
+        restaurant.menu.name = "Menu Principal"
+        restaurant.menu.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a sagittis magna."
+        populateMenu(restaurant.menu)
+        return restaurant
+    }
 
+    def populateMenu(menu) {        
+        def menuSection
+        def sectionNames = ["Aperitivos", "Refeição", "Carnes", "Saladas", "Bebidas", "Sobremesas"]
         for(element in sectionNames) {
-            menu.addToSections(name: element)
+            menuSection = new MenuSection(name: element)
+            populateDishes(menuSection)            
+            menu.addToSections(menuSection)
         }
     }
+
+    def populateDishes(menuSection) {        
+        def dishesNames = ["Prato 01", "Prato 02", "Prato 03", "Prato 04", "Prato 05", "Prato 06"]
+        def random = new Random()
+        def price = 0
+        def description
+        for(element in dishesNames) {
+            price = Math.abs(random.nextInt() % 100 + 1)
+            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            menuSection.addToDishes(name: element, price: price, cuisine: menuSection.name, description: description)
+        }
+    }    
 }
